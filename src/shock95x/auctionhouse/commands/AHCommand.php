db6209ede7135e308a04f2bb8e7dc32e438ffc5a
NdaruAjaa <120300909+NdaruAjaa@users.noreply.github.com>
2025-03-01 15:51:05 +0700

    Update AHCommand.php

--- src/shock95x/auctionhouse/commands/AHCommand.php
+++ src/shock95x/auctionhouse/commands/AHCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace shock95x\auctionhouse\commands;
@@ -7,8 +8,9 @@ use CortexPE\Commando\BaseCommand;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use shock95x\auctionhouse\commands\subcommand\AboutCommand;
-use shock95x\auctionhouse\commands\subcommand\admin\AdminCommand;
+use shock95x\auctionhouse\commands\subcommand\AdminCommand;
 use shock95x\auctionhouse\commands\subcommand\CategoryCommand;
+use shock95x\auctionhouse\commands\subcommand\ConvertCommand;
 use shock95x\auctionhouse\commands\subcommand\ExpiredCommand;
 use shock95x\auctionhouse\commands\subcommand\ListingsCommand;
 use shock95x\auctionhouse\commands\subcommand\ReloadCommand;
@@ -16,24 +18,26 @@ use shock95x\auctionhouse\commands\subcommand\SellCommand;
 use shock95x\auctionhouse\commands\subcommand\ShopCommand;
 use shock95x\auctionhouse\menu\ShopMenu;
 use shock95x\auctionhouse\menu\type\AHMenu;
+use function count;
 
-class AHCommand extends BaseCommand {
+class AHCommand extends BaseCommand{
 
-	protected function prepare() : void {
+	protected function prepare() : void{
 		$this->setPermission("auctionhouse.command");
-		$this->registerSubCommand(new ShopCommand($this->plugin, "shop", "Shows AH shop menu"));
-		$this->registerSubCommand(new AdminCommand($this->plugin, "admin", "Opens AH admin menu"));
-		$this->registerSubCommand(new SellCommand($this->plugin, "sell", "Sell item in hand to the AH"));
-		$this->registerSubCommand(new CategoryCommand($this->plugin, "category", "Opens category menu"));
-		$this->registerSubCommand(new ListingsCommand($this->plugin, "listings", "Shows player listings"));
-		$this->registerSubCommand(new ExpiredCommand($this->plugin, "expired", "Shows expired listings"));
-		$this->registerSubCommand(new ReloadCommand($this->plugin, "reload", "Reload plugin configuration files"));
-		$this->registerSubCommand(new AboutCommand($this->plugin, "about", "Plugin information"));
+		$this->registerSubCommand(new ShopCommand("shop", "Shows AH shop menu"));
+		$this->registerSubCommand(new AdminCommand("admin", "Opens AH admin menu"));
+		$this->registerSubCommand(new SellCommand("sell", "Sell item in hand to the AH"));
+		$this->registerSubCommand(new CategoryCommand("category", "Opens category menu"));
+		$this->registerSubCommand(new ListingsCommand("listings", "Shows player listings"));
+		$this->registerSubCommand(new ExpiredCommand("expired", "Shows expired listings"));
+		$this->registerSubCommand(new ReloadCommand("reload", "Reload plugin configuration files"));
+		$this->registerSubCommand(new AboutCommand("about", "Plugin information"));
+		$this->registerSubCommand(new ConvertCommand("convert", "Legacy DB conversion"));
 	}
 
-	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
-		if(count($args) == 0 && $sender instanceof Player) {
-			(new ShopMenu($sender))->open();
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args) : void{
+		if(count($args) == 0 && $sender instanceof Player){
+			AHMenu::open(new ShopMenu($sender));
 		}
 	}
 }
