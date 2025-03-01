0ac2ddb1613a7312f19d5de368bb6caf894d0029
NdaruAjaa <120300909+NdaruAjaa@users.noreply.github.com>
2025-03-01 15:50:18 +0700

    Update ManageListingMenu.php

--- src/shock95x/auctionhouse/menu/admin/ManageListingMenu.php
+++ src/shock95x/auctionhouse/menu/admin/ManageListingMenu.php
@@ -1,90 +1,87 @@
 <?php
+
 declare(strict_types=1);
 
 namespace shock95x\auctionhouse\menu\admin;
 
-use pocketmine\block\tile\Container;
-use pocketmine\data\bedrock\item\SavedItemStackData;
 use pocketmine\inventory\Inventory;
 use pocketmine\item\Item;
 use pocketmine\block\VanillaBlocks;
-use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
-use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use shock95x\auctionhouse\AHListing;
-use shock95x\auctionhouse\AuctionHouse;
+use shock95x\auctionhouse\database\storage\DataStorage;
 use shock95x\auctionhouse\event\AuctionEndEvent;
 use shock95x\auctionhouse\menu\type\AHMenu;
 use shock95x\auctionhouse\utils\Locale;
-use SOFe\AwaitGenerator\Await;
+use shock95x\auctionhouse\utils\Utils;
+use function implode;
+use function preg_filter;
+use function str_ireplace;
 
-class ManageListingMenu extends AHMenu {
+class ManageListingMenu extends AHMenu{
 
-	const INDEX_COPY = 47;
-	const INDEX_REMOVE = 49;
-	const INDEX_DELETE = 51;
+	const INDEX_DUPLICATE = 38;
+	const INDEX_STATUS = 40;
+	const INDEX_DELETE = 42;
 
-	public function __construct(Player $player, AHListing $listing) {
+	public function __construct(Player $player, AHListing $listing){
 		$this->setName(Locale::get($player, "manage-listing-name"));
 		$this->setListings([$listing]);
 		parent::__construct($player);
 	}
 
-	public function renderButtons(): void {
-		$this->inventory->setItem(self::INDEX_COPY,
-			VanillaBlocks::HOPPER()->asItem()->setCustomName(TextFormat::RESET . Locale::get($this->player, "copy-item")));
-		$this->inventory->setItem(self::INDEX_REMOVE,
-			VanillaItems::POISONOUS_POTATO()->setCustomName(TextFormat::RESET . Locale::get($this->player, "return-item")));
-		$this->inventory->setItem(self::INDEX_DELETE,
-			VanillaBlocks::BARRIER()->asItem()->setCustomName(TextFormat::RESET . Locale::get($this->player, "delete-item")));
+	public function renderButtons() : void{
+		parent::renderButtons();
+		$listing = $this->getListings()[0];
+		$duplicateItem = VanillaBlocks::EMERALD()->asItem()->setCustomName(TextFormat::RESET . Locale::get($this->player, "duplicate-item"));
+		$status = Locale::get($this->player, $listing->isExpired() ? "status-expired" : "status-active");
+		$listingStatus = VanillaBlocks::GOLD()->asItem()->setCustomName(str_ireplace("{STATUS}", $status, implode("\n", preg_filter('/^/', TextFormat::RESET, Locale::get($this->player, "listing-status")))));
+		$deleteItem = VanillaBlocks::REDSTONE()->asItem()->setCustomName(TextFormat::RESET . Locale::get($this->player, "delete-item"));
+
+		$this->inventory->setItem(self::INDEX_DUPLICATE, $duplicateItem);
+		$this->inventory->setItem(self::INDEX_STATUS, $listingStatus);
+		$this->inventory->setItem(self::INDEX_DELETE, $deleteItem);
 	}
 
-	public function renderListings(): void {
-		$listingItem = $this->getListings()[0]->getItem();
-		if($itemsTag = $listingItem->getNamedTag()?->getListTag(Container::TAG_ITEMS)) { // Shulker items
-			foreach($itemsTag->getIterator() as $itemTag) {
-				$this->inventory->setItem($itemTag->getByte(SavedItemStackData::TAG_SLOT), Item::nbtDeserialize($itemTag));
-			}
-			$this->inventory->setItem(31, $listingItem);
-		} else {
-			$this->inventory->setItem(22, $listingItem);
-		}
+	public function renderListings() : void{
+		$this->inventory->setItem(22, $this->getListings()[0]->getItem());
 	}
 
-	public function handle(Player $player, Item $itemClicked, Inventory $inventory, int $slot): bool {
-		Await::f2c(function() use ($player, $slot) {
-			$database = AuctionHouse::getInstance()->getDatabase();
-			/** @var AHListing $listing */
-			$listing = yield from Await::promise(fn($resolve) => $database->getListingById($this->getListings()[0]->getId(), $resolve));
-			switch($slot) {
-				case self::INDEX_COPY:
-					$player->getInventory()->addItem($listing->getItem());
-					break;
-				case self::INDEX_REMOVE:
-					if($listing == null || $listing->isExpired()) {
-						$this->returnMenu?->send($player);
-						break;
-					}
-					yield from $database->setExpiredAsync($listing->getId());
-					(new AuctionEndEvent($listing, AuctionEndEvent::CANCELLED))->call();
-					if($seller = Server::getInstance()->getPlayerByUUID($listing->getSellerUUID())) {
-						$item = $listing->getItem();
-						$seller->sendMessage(str_ireplace(["{ITEM}", "{AMOUNT}"], [$item->getName(), $item->getCount()], Locale::get($seller, "returned-item", true)));
-					}
-					$this->returnMenu?->send($player);
-					break;
-				case self::INDEX_DELETE:
-					if($listing == null) {
-						$this->returnMenu?->send($player);
-						break;
-					}
-					yield from $database->removeListingAsync($listing->getId());
+	public function handle(Player $player, Item $itemClicked, Inventory $inventory, int $slot) : bool{
+		$listing = $this->getListings()[0];
+		switch($slot){
+			case self::INDEX_DUPLICATE:
+				$player->getInventory()->addItem($listing->getItem());
+				break;
+			case self::INDEX_STATUS:
+				if($listing->isExpired()){
+					$listing->setExpired(false);
+					DataStorage::getInstance()->setExpired($listing, value: false);
+					$listing->setEndTime(Utils::getEndTime());
+				}else{
+					$listing->setExpired();
 					(new AuctionEndEvent($listing, AuctionEndEvent::ADMIN_REMOVED))->call();
-					$this->returnMenu?->send($player);
-					break;
-			}
-		});
-		return true;
+				}
+				$this->renderButtons();
+				break;
+			case self::INDEX_DELETE:
+				DataStorage::getInstance()->removeListing($listing);
+				(new AuctionEndEvent($listing, AuctionEndEvent::ADMIN_PURGED))->call();
+				self::open(new AdminMenu($player, false));
+				break;
+		}
+		return parent::handle($player, $itemClicked, $inventory, $slot);
+	}
+
+    /**
+     * Not need for this class, cause this class extends AHMenu, and AHMenu already have method
+     * @deprecated
+     * @param Player $player
+     * @return void
+     * @author comment by XackiGiFF
+     */
+	public function onClose(Player $player) : void{
+		parent::onClose($player);
 	}
 }
